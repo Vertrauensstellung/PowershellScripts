26bc23668f6a4b471718fbcb4268dcdf38aa5043
Vertrauensstellung <33381081+Vertrauensstellung@users.noreply.github.com>
2021-03-29 23:48:43 +0200

    Create Remote_Speech_Laptop.ps1

---
+++ Remote_Speech_Laptop.ps1
@@ -0,0 +1,8 @@
+﻿### Hostname to variable ###
+$Computer = "hostname"
+### Create Remote PSSession (Remote PS needs to be enabled) ###
+New-PSSession -ComputerName $Computer
+### Invoke command remote ###
+Invoke-Command -ComputerName $Computer -ScriptBlock {Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.Speak("Remote command. Can you here me?")}
