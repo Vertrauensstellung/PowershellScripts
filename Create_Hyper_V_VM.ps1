821f4d378eedb4003f74dfa3bff7a588b91014a8
Vertrauensstellung <33381081+Vertrauensstellung@users.noreply.github.com>
2021-04-05 17:02:11 +0200

    Create Create_Hyper_V_VM.ps1

---
+++ Create_Hyper_V_VM.ps1
@@ -0,0 +1,20 @@
+﻿### Create VM with Hyper-V ###
+### https://docs.microsoft.com/en-us/virtualization/hyper-v-on-windows/quick-start/try-hyper-v-powershell ###
+### Insert Variables ###
+$vmname = 'Windows001'
+$isopath = 'C:\Users\Seal\Downloads\Windows2010.iso'
+$cores = 4
+### Create VM ###
+New-VM -Name $vmname -MemoryStartupBytes 1024MB -Path $Env:USERPROFILE\HyperV\$vmname
+### Create VHD ###
+New-VHD -Path $Env:USERPROFILE\HyperV\$vmname.vhdx -SizeBytes 60GB -Dynamic
+### Add VHD to VM ###
+Add-VMHardDiskDrive -VMName $vmname -Path $Env:USERPROFILE\HyperV\$vmname.vhdx
+### Set ISO as Startdrive ###
+Set-VMDvdDrive -VMName $vmname -ControllerNumber 1 -Path $isopath
+### Set Dynamic RAM ###
+Set-VMMemory -VMName $vmname -DynamicMemoryEnabled $true -StartupBytes 1024MB -MinimumByte 1024MB
+### Set cpu cores ###
+Set-VMProcessor $vmname -Count $cores
+### Start VM ###
+Start-VM –Name $vmname
