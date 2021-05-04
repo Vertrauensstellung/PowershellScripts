c161de4c296ab7c3cab346d34bcb568e5ae84206
Vertrauensstellung <33381081+Vertrauensstellung@users.noreply.github.com>
2021-05-05 01:16:17 +0200

    Create Clean_Windows10.ps1

---
+++ Clean_Windows10.ps1
@@ -0,0 +1,10 @@
+﻿### Only working with one drive called C ###
+### Get free disk space before cleanup ###
+$before = Get-PSDrive | Where Name -EQ 'C' | Select-Object Free -ExpandProperty Free
+$before = $before /1GB
+### Clean recycle-bin ###
+Clear-RecycleBin -Force
+
+### Get free disk space after cleanup ###
+$after = Get-PSDrive | Where Name -EQ 'C' | Select-Object Free -ExpandProperty Free
+$after = $after /1GB
