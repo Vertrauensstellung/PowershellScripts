4fff4bb99414cb4bdc7336d81bff5c07568e35d7
Vertrauensstellung <33381081+Vertrauensstellung@users.noreply.github.com>
2021-05-13 09:49:31 +0200

    Create Read_Out_PC_Details.ps1

---
+++ Read_Out_PC_Details.ps1
@@ -0,0 +1,9 @@
+﻿### Read out pc specs ###
+Write-Host Prozessor -ForegroundColor Green
+Write-Host Festplatte -ForegroundColor Green
+(Get-WmiObject -Class:Win32_ComputerSystem).Manufacturer
+(Get-WmiObject -Class:Win32_ComputerSystem).SystemFamily
+(Get-WmiObject -Class:Win32_ComputerSystem).Model
+Write-Host Arbeitsspeicher -ForegroundColor Green
+(Get-WmiObject -Class:Win32_ComputerSystem).TotalPhysicalMemory
+(Get-WmiObject -Class:Win32_BIOS).SerialNumber
