2688b45130935fa147fd69597893bfe46c99d901
Vertrauensstellung <33381081+Vertrauensstellung@users.noreply.github.com>
2021-05-13 18:13:06 +0200

    Create Remove_Bloatware.ps1

---
+++ Remove_Bloatware.ps1
@@ -0,0 +1,15 @@
+﻿### Remove bloatware after fresh install ###
+
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+Get-AppxPackage *officehub* | Remove-AppxPackage
+Get-AppxPackage *skypeapp* | Remove-AppxPackage
+Get-AppxPackage *getstarted* | Remove-AppxPackage
+Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+Get-AppxPackage *bingfinance* | Remove-AppxPackage
+Get-AppxPackage *bingnews* | Remove-AppxPackage
+Get-AppxPackage *onenote* | Remove-AppxPackage
+Get-AppxPackage *people* | Remove-AppxPackage
+Get-AppxPackage *windowsphone* | Remove-AppxPackage
+Get-AppxPackage *bingsports* | Remove-AppxPackage
+Get-AppxPackage *xboxapp* | Remove-AppxPackage
