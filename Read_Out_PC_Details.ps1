382d33a9593e9960f65da551a348e2fa8484f58c
Vertrauensstellung <33381081+Vertrauensstellung@users.noreply.github.com>
2021-05-15 14:02:21 +0200

    Update Read_Out_PC_Details.ps1

--- Read_Out_PC_Details.ps1
+++ Read_Out_PC_Details.ps1
@@ -5,6 +5,9 @@ Write-Host PC-Info -ForegroundColor Green
 (Get-WmiObject -Class:Win32_ComputerSystem).Model
 (Get-WmiObject -Class:Win32_BIOS).SerialNumber
 Write-Host CPU -ForegroundColor Green
+(Get-WmiObject -Class Win32_Processor).Name
 Write-Host Drive -ForegroundColor Green
+(Get-WmiObject -class win32_logicaldisk).Size / 1GB
+(Get-WmiObject -class win32_logicaldisk).VolumeSerialNumber
 Write-Host RAM -ForegroundColor Green
-(Get-WmiObject -Class:Win32_ComputerSystem).TotalPhysicalMemory
+(Get-WmiObject -Class:Win32_ComputerSystem).TotalPhysicalMemory / 1GB
