78ca77689366dd31ce4bca1b5627c80d75d56b6f
Vertrauensstellung <33381081+Vertrauensstellung@users.noreply.github.com>
2021-07-12 19:42:07 +0200

    Create Fix_iExplorer.exe.ps1

---
+++ Fix_iExplorer.exe.ps1
@@ -0,0 +1,6 @@
+﻿### iexplorer.exe 2012 R2 Fix ###
+New-Item -Path 'HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\' -Name FEATURE_ENABLE_PRINT_INFO_DISCLOSURE_FIX  –Force
+Get-Item -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_ENABLE_PRINT_INFO_DISCLOSURE_FIX" | New-ItemProperty -Name iexplore.exe -Value 1
+
+New-Item -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Internet Explorer\Main\FeatureControl\' -Name FEATURE_ENABLE_PRINT_INFO_DISCLOSURE_FIX  –Force
+Get-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_ENABLE_PRINT_INFO_DISCLOSURE_FIX" | New-ItemProperty -Name iexplore.exe -Value 1
